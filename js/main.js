1f5eafec96dbcd2c3d3af29fcd0b95901a8e5e3b
Lu Li <luli12058@gmail.com>
2019-04-16 22:48:24 -0400

    lu-edit April 16th
    add new task to list with checkBox

--- js/main.js
+++ js/main.js
@@ -1,47 +1,82 @@
-let id = 000;
-let sport = 'Swimming';
-let number = 00;
-let unit = 'minutes';
-let duration = number, unit;
-let completion = 30%;
+//let id = 000;
+//let sport = 'Swimming';
+//let number = 00;
+//let unit = 'minutes';
+//let duration = number, unit;
+//let completion = 30%;
+//
+//
+//let task_list = [
+//  {  //task, index 0
+//    id: 001,
+//    sport: 'Swimming',
+//    duration: {
+//      number: 30,
+//      unit: 'minutes'
+//    }
+//    completion: 30%,
+//  },
+//  {  //task, index 2
+//    id: 002,
+//    sport: 'Jogging',
+//    duration: {
+//      number: 50,
+//      unit: 'minutes'
+//    }
+//    completion: 60%,
+//  },
+//  {  //task, index 3
+//    id: 003,
+//    sport: 'Playing basketball',
+//    duration: {
+//      number: 40,
+//      unit: 'minutes'
+//    }
+//    completion: 70%,
+//  },
+//  {  //task, index 4
+//    id: 004,
+//    sport: 'Push-ups',
+//    duration: {
+//      number: 20,
+//      unit: 'minutes'
+//    }
+//    completion: 110%,
+//  }
+//];
+//
+//console.log(task_list[1].id);
 
-let task_list = [
-  {  //task, index 0
-    id: 001,
-    sport: 'Swimming',
-    duration: {
-      number: 30,
-      unit: 'minutes'
-    }
-    completion: 30%,
+function newElement() {
+  var ol = document.getElementById("task_list");
+  var checkbox = document.createElement('input'); 
+              
+            // Assigning the attributes 
+            // to created checkbox 
+            checkbox.type = "checkbox"; 
+            checkbox.name = "name"; 
+            checkbox.value = "value"; 
+            checkbox.id = "id"; 
+  
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("newTask").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(checkbox);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("task_list").appendChild(li);
   }
-  {  //task, index 2
-    id: 002,
-    sport: 'Jogging',
-    duration: {
-      number: 50,
-      unit: 'minutes'
-    }
-    completion: 60%,
-  }
-  {  //task, index 3
-    id: 003,
-    sport: 'Playing basketball',
-    duration: {
-      number: 40,
-      unit: 'minutes'
-    }
-    completion: 70%,
-  }
-  {  //task, index 4
-    id: 004,
-    sport: 'Push-ups',
-    duration: {
-      number: 20,
-      unit: 'minutes'
-    }
-    completion: 110%,
-  }
-];
+  document.getElementById("newTask").value = "";
 
-console.log(task_list[1].id)
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+
+
